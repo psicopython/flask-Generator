2c03b397bcb7af29c8403f9cefd6f46e0af06dde
Etho Domingues <ethodomingues3@gmail.com>
2020-07-28 14:30:28 -0300

    melhorando o app²

--- GerarApp/__main__.py
+++ GerarApp/__main__.py
@@ -1,97 +1,29 @@
 import os
 import shutil
 from time import sleep
-
+from app import *
 
 try:
-	erro = False
 	while True:
 		os.system('cls' if os.name == 'nt' else 'clear')
-		if erro:
-			print("\n\n\033[31mNome do pacote ja existe!\033[m\n")
-			erro = False
-		pkg = input("nome do package: ")
-		if not os.path.exists(pkg):
-			dados ={"c":"cod/c.py",
-				"m":"cod/db.py",
-				"v":"cod/vw.py",
-				"i":"cod/init.py",
-				"re":'requirements.txt',
-				"bhtml":"html/base.html",
-				"inhtml":"html/index.html"
-				}
-			dados2={
-				"c":"cod/cl.py",
-				"m":"cod/dbl.py",
-				"v":"cod/vwl.py"
-				}
-			
-			
-			arq = {"c":"config.py",
-				"i":"__init__.py",
-				"m":"model.py",
-				"v":"views.py"
-				}
-			
-			pas = {"c":"config",
-				"m":"model","s":"static",
-				"t":"templates","v":"views"}
-			
-			for c,v in dados.items():
-				with open(f"GerarApp/base/{v}","r") as i:
-					dados[c] = i.read()
-			
-			for c,v in dados2.items():
-				with open(f"GerarApp/base/{v}","r") as i:
-					dados2[c] = i.read().replace('€|€',pkg.upper())
-			
-			
-			
-			os.mkdir(pkg)
-			os.mkdir(pkg+"/app")
-			os.system(f"""
-				echo "export FLASK_APP=app.app:create_app\nexport FLASK_ENV=development" > {pkg}/.env
-				echo "{dados['i']}  " > {pkg}/app/app.py
-				echo "{dados['re']} " > {pkg}/requirements.txt
-				echo "" > {pkg}/app/{arq['i']}
-			""")
-			for i,v in pas.items():
-				if i == "s":
-					os.mkdir(f"{pkg}/app/{v}")
-					os.mkdir(f"{pkg}/app/{v}/js")
-					os.mkdir(f"{pkg}/app/{v}/css")
-					os.system(f"echo '' >{pkg}/app/{v}/js/custom.js")
-					os.system(f"echo '' >{pkg}/app/{v}/css/custom.css")
-					
-				elif i == "t":
-					os.mkdir(f"{pkg}/app/{v}")
-					os.system(f"echo '{dados['bhtml']}'  > {pkg}/app/{v}/base.html")
-					os.system(f"echo '{dados['inhtml']}' > {pkg}/app/{v}/index.html")
-				else:
-					os.mkdir(f"{pkg}/app/{v}")
-					os.system(f"echo '{dados[i]}' > {pkg}/app/{v}/{arq['i']}")
-					os.system(f"echo '{dados2[i]}' > {pkg}/app/{v}/{arq[i]}")
-			
-			
-			os.system('cls' if os.name == 'nt' else 'clear')
-			print('\033[33m|•',end='',flush=True)
-			for c in range(0,20):
-				sleep(.005)
-				print(' •',end='',flush=True)
-			print('| 100%\033[m')
-			sleep(.2)
-			os.system('cls' if os.name == 'nt' else 'clear')
-		    
-			print(f"\n\033[32mAplicação FLASK criada com sucesso!\033[m\n")
+		print("""\033[36mQUAL TIPO DE APP:
+-----------------
+\033[32m[ 1 ] APP Padrão\n[ X ] Cancelar\033[m""")
+		opt = input('$ ')
+		if opt.upper() == 'X':
 			break
+		elif opt.isdigit():
+			opt = int(opt)
+			if opt == 1:
+				appc()
+				break
 		else:
 			erro = True
 			continue
 
 except KeyboardInterrupt:
-	print('\n\033[31mOperação Abortada! pelo usuário\033[m')
+	print('\n\033[31mOperação Abortada pelo usuário!\033[m')
 except EOFError:
 	print('\n\n\033[31mOperação Abortada pelo usuário!\033[m')
 
-#except IOError:
-#	print('\n\033[31mOperação Abortada pelo usuário!\033[m')
+
---
+++ GerarApp/app/__init__.py
@@ -0,0 +1,82 @@
+import os
+from time import sleep
+
+def appc():
+	erro = False
+	while True:
+		os.system('cls' if os.name == 'nt' else 'clear')
+		if erro:
+			print("\n\n\033[31mNome do pacote ja existe!\033[m\n")
+			erro = False
+		pkg = input("nome do package: ")
+		if not os.path.exists(pkg):
+			dados ={
+				"c":"cod/c.py",
+				"m":"cod/db.py",
+				"v":"cod/vw.py",
+				"i":"cod/init.py",
+				"re":'requirements.txt',
+				"toml":"toml.py",
+				"bhtml":"html/base.html",
+				"inhtml":"html/index.html"
+				}
+			dados2={
+				"c":"cod/cl.py",
+				"m":"cod/dbl.py",
+				"v":"cod/vwl.py"
+				}
+			
+			
+			arq = {
+				"c":"config.py",
+				"i":"__init__.py",
+				"m":"model.py",
+				"v":"views.py"
+				}
+			
+			pas = {
+				"c":"config",
+				"m":"model","s":"static",
+				"t":"templates","v":"views"}
+			
+			
+			for c,v in dados.items():
+				with open(f"GerarApp/base/{v}","r") as i:
+					dados[c] = i.read()
+			
+			for c,v in dados2.items():
+				with open(f"GerarApp/base/{v}","r") as i:
+					dados2[c] = i.read().replace('€|€',pkg.upper())
+			
+			
+			
+			os.mkdir(pkg)
+			os.mkdir(pkg+"/app")
+			os.system(f"""
+				echo "export FLASK_APP=app.app:create_app\nexport FLASK_ENV=development" > {pkg}/.env
+				echo "{dados['i']}  " > {pkg}/app/app.py
+				echo "{dados['re']} " > {pkg}/requirements.txt
+				echo "{dados['toml'] > pyproject.toml}"
+				echo "" > {pkg}/app/{arq['i']}
+				""")
+			for i,v in pas.items():
+				if i == "s":
+					os.mkdir(f"{pkg}/app/{v}")
+					os.mkdir(f"{pkg}/app/{v}/js")
+					os.mkdir(f"{pkg}/app/{v}/css")
+					os.system(f"echo '' >{pkg}/app/{v}/js/custom.js")
+					os.system(f"echo '' >{pkg}/app/{v}/css/custom.css")
+					
+				elif i == "t":
+					os.mkdir(f"{pkg}/app/{v}")
+					os.system(f"echo '{dados['bhtml']}'  > {pkg}/app/{v}/base.html")
+					os.system(f"echo '{dados['inhtml']}' > {pkg}/app/{v}/index.html")
+				else:
+					os.mkdir(f"{pkg}/app/{v}")
+					os.system(f"echo '{dados[i]}' > {pkg}/app/{v}/{arq['i']}")
+					os.system(f"echo '{dados2[i]}' > {pkg}/app/{v}/{arq[i]}")
+			
+
+			os.system('cls' if os.name == 'nt' else 'clear')
+			print(f"\n\033[32mAplicação FLASK criada com sucesso!\033[m\n")
+			break
